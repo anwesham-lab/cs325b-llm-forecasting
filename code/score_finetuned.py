71d777073f19ad3eda7d72d8ddd821050a2a37b4
Shreyas Lakhtakia <shreyaslakhtakia@gmail.com>
2023-11-13 15:26:22 -0800

    reparam for growth data

--- code/score_finetuned.py
+++ code/score_finetuned.py
@@ -8,20 +8,38 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 ###################### FILL IN THE RIGHT VALUES HERE  ######################
 
 # input file - SPECIFY
-input_csv_filename = "../data/gdp/test_cleaned_gdp.csv" #'../data/gdp/cleaned_gdp_df_countries.csv'
 
-# system message to "prepare" the llm
-nom_prep_msg = "You are a prediction agent predicting the GDP for a country in a given year when provided an input detailing the country's GDP for the last 10 years."
-norm_prep_msg = "You are a prediction agent predicting the GDP for a country in a given year when provided an input detailing the country's GDP for the last 10 years. The GDP has been normalized so that it is always between 0 and 10."
-prep_msg = nom_prep_msg
+'''
+# if working with GDP
+input_csv_filename = "../data/gdp/test_cleaned_gdp.csv" #'../data/gdp/cleaned_gdp_df_countries.csv'
+# output_csv_filename = "../data/gdp/test_cleaned_gdp_finetuned_predictions.csv"
 
 # Train / test splits
 test_windows_start = 2012
 predict_after_years = 10
+'''
+
+'''
+# if working with GDP growth
+'''
+input_csv_filename = "../data/gdp/rounded_cleaned_gdp_growth_df.csv" #'../data/gdp/cleaned_gdp_df_countries.csv'
+output_csv_filename = "../data/gdp/test_cleaned_gdp_growth_finetuned_predictions.csv"
+
+# Train / test splits
+test_windows_start = 2010
+predict_after_years = 12
+
+# system message to "prepare" the llm
+nom_prep_msg = "You are a prediction agent predicting the GDP for a country in a given year when provided an input detailing the country's GDP for the last 10 years."
+norm_prep_msg = "You are a prediction agent predicting the GDP for a country in a given year when provided an input detailing the country's GDP for the last 10 years. The GDP has been normalized so that it is always between 0 and 10."
+growth_prep_msg = "You are a prediction agent predicting the GDP growth rates for a country in a future year when provided an input detailing the country's growth rate in real GDP for the last 10 years."
+prep_msg = growth_prep_msg
+
+
 
 # limit how many predictions to make
-PREDICTION_LIMIT = 1000 # if you want to just test the code, set to 5
-debug_mode = False
+PREDICTION_LIMIT = 10 # if you want to just test the code, set to 5
+debug_mode = True
 
 if PREDICTION_LIMIT < 50:
     debug_mode = True
@@ -40,7 +58,11 @@ finetuned_predictions = []
 true_values = []
 countries = []
 
-def query_model(row, window_start):
+'''
+For predicting gdp values directly
+'''
+
+def query_model_gdp(row, window_start, prep_msg=nom_prep_msg):
     country_name = row['country'] # loop through each country
     
     # Create sliding window prompts
@@ -78,11 +100,54 @@ def query_model(row, window_start):
 
     return country_name, finetuned_prediction, true_value
 
+'''
+For predicting GDP growth rates
+'''
+
+def query_model_gdp_growth(row, window_start, prep_msg=growth_prep_msg):
+    '''
+    to be used to query a model finetuned to make gdp growth 
+    predictions in % terms on a 3 year horizon
+    '''
+    country_name = row['country'] # loop through each country
+    
+    # Create sliding window prompts
+    i = window_start
+    prediction_year = i + predict_after_years
+
+    # GDP growth prompts
+    input_message = f"The country of interest is: {country_name} and growth rates in percent of real GDP seen in previous years is {i}: {row[str(i)]}, {i+1}: {row[str(i+1)]}, {i+2}: {row[str(i+2)]}, {i+3}: {row[str(i+3)]}, {i+4}: {row[str(i+4)]}, {i+5}: {row[str(i+5)]}, {i+6}: {row[str(i+6)]}, {i+7}: {row[str(i+7)]}, {i+8}: {row[str(i+8)]}, {i+9}: {row[str(i+9)]}"
+    task_message = f". Predict the GDP growth rate (in percent) for {country_name} in {prediction_year}: "
+
+    # what the LLM should return
+    true_value = float(row[str(i+predict_after_years)])
+
+    # query the model
+    completion = openai.ChatCompletion.create(
+      model="ft:gpt-3.5-turbo-1106:personal::8IU7aKVL",
+      messages=[
+        {"role": "system", "content": system_message},
+        {"role": "user", "content": input_message + task_message}
+      ]
+    )
+    finetuned_prediction_dict = dict()
+    finetuned_prediction_dict = completion.choices[0].message
+    finetuned_prediction = float(finetuned_prediction_dict['content'])
+
+    # preview results
+    if debug_mode:
+        print(input_message)
+        print(task_message)
+    print(country_name, finetuned_prediction, true_value)
+
+    return country_name, finetuned_prediction, true_value
+
 # loop through all countries and return results
 prediction_idx = 0
 for row in data:
     if prediction_idx < PREDICTION_LIMIT:
-        country_name, y_hat, y_true = query_model(row, test_windows_start) # Extract relevant data for that country and start year combination
+        # country_name, y_hat, y_true = query_model_gdp(row, test_windows_start) # Extract relevant data for that country and start year combination - GDP 
+        country_name, y_hat, y_true = query_model_gdp_growth(row, test_windows_start) # Extract relevant data for that country and start year combination - GDP Growth
         countries.append(country_name)
         true_values.append(y_true)
         finetuned_predictions.append(y_hat)
@@ -93,4 +158,4 @@ results_df = pd.DataFrame()
 results_df['country'] = countries
 results_df['predicted_gdp'] = finetuned_predictions
 results_df['observed_gdp'] = true_values
-results_df.to_csv("../data/gdp/test_cleaned_gdp_finetuned_predictions.csv")
+results_df.to_csv(output_csv_filename)
