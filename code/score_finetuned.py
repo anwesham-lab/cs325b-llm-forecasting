ecddcc80df9c839039acdf5ceeee6025398c6eaf
Shreyas Lakhtakia <shreyaslakhtakia@gmail.com>
2023-12-04 20:50:07 -0800

    update score finetuned to accommodate zero shot

--- code/score_finetuned.py
+++ code/score_finetuned.py
@@ -16,40 +16,43 @@ if ZERO_SHOT:
 '''
 # if working with GDP
 input_csv_filename = "../data/gdp/test_cleaned_gdp.csv" #'../data/gdp/cleaned_gdp_df_countries.csv'
-# output_csv_filename = "../data/gdp/test_cleaned_gdp" + prediction_mode_string + "predictions.csv"
+# output_csv_filename = " ../data/gdp/test_cleaned_gdp" + prediction_mode_string + "predictions.csv"
+output_csv_filename = "test_cleaned_gdp" + prediction_mode_string + "predictions.csv"
 
 # Train / test splits
 test_windows_start = 2012
 predict_after_years = 10
-'''
+
 
 '''
 # if working with GDP growth
-'''
+
 
 # Train / test splits
-prediction_horizon = 1 #3
+prediction_horizon = 1 #1 #3
 if prediction_horizon == 3:
     test_windows_start = 2010
     predict_after_years = 12
 elif prediction_horizon == 1:
-    test_windows_start = 2012 #2010
-    predict_after_years = 10 #12
+    test_windows_start = 2010 #2012 #2010
+    predict_after_years = 12 #10 #12
 else: 
     print("bad parameters")
 
-input_csv_filename = "../data/gdp/rounded_cleaned_gdp_growth_df.csv" #'../data/gdp/cleaned_gdp_df_countries.csv'
+input_csv_filename = "../data/gdp/rounded_cleaned_gdp_growth_df.csv" 
 output_csv_filename = "../data/gdp/test_cleaned_gdp_growth" + prediction_mode_string + "predictions_" + str(prediction_horizon) + "yr.csv"
-
+output_csv_filename = "test_cleaned_gdp_growth" + prediction_mode_string + "predictions_" + str(prediction_horizon) + "yr.csv"
 
 # system message to "prepare" the llm
 nom_prep_msg = "You are a prediction agent predicting the GDP for a country in a given year when provided an input detailing the country's GDP for the last 10 years."
+zero_shot_nom_prep_msg = "You are a prediction agent predicting the GDP for a country in a given year when provided an input detailing the country's GDP for the last 10 years. Respond using just a numeric prediction. Don't try to help me calculate this myself or give caveats, simply return a single number."
 norm_prep_msg = "You are a prediction agent predicting the GDP for a country in a given year when provided an input detailing the country's GDP for the last 10 years. The GDP has been normalized so that it is always between 0 and 10."
 growth_prep_msg = "You are a prediction agent predicting the GDP growth rates for a country in a future year when provided an input detailing the country's growth rate in real GDP for the last 10 years."
-prep_msg = growth_prep_msg
+zero_shot_growth_prep_msg = "You are a prediction agent predicting the GDP growth rates for a country in a future year when provided an input detailing the country's growth rate in real GDP for the last 10 years. Respond using just a numeric prediction. Don't try to help me calculate this myself or give caveats, simply return a single number."
+prep_msg = zero_shot_growth_prep_msg
 
 # limit how many predictions to make
-PREDICTION_LIMIT = 500 # if you want to just test the code, set to 5
+PREDICTION_LIMIT = 2000 # if you want to just test the code, set to 5
 debug_mode = False
 
 if PREDICTION_LIMIT < 50:
@@ -73,7 +76,7 @@ countries = []
 For predicting gdp values directly
 '''
 
-def query_model_gdp(row, window_start, prep_msg=nom_prep_msg, zero_shot=False):
+def query_model_gdp(row, window_start, prep_msg, zero_shot=False):
     country_name = row['country'] # loop through each country
     
     # Create sliding window prompts
@@ -95,6 +98,14 @@ def query_model_gdp(row, window_start, prep_msg=nom_prep_msg, zero_shot=False):
         model="gpt-3.5-turbo"
     else:
         model="ft:gpt-3.5-turbo-0613:personal::8Fmg2rfz"
+
+    # messages to user about what exactly is happening
+    if debug_mode:
+        print(input_message)
+        print(task_message)
+        print("Model being used for predictions is ", model)
+        print("Predictions are being made for year ", prediction_year)
+        print("First year for which data is being prompted in is ", window_start)
         
     # query the model
     completion = openai.ChatCompletion.create(
@@ -112,17 +123,14 @@ def query_model_gdp(row, window_start, prep_msg=nom_prep_msg, zero_shot=False):
     try:
         float_value_out = float(finetuned_prediction_dict['content'])
     except ValueError:
-        print(f"Error: Invalid value encountered. Could not convert '{input_str}' to a float.")
+        print(f"Error: Invalid value encountered. Could not convert '{finetuned_prediction_dict['content']}' to a float.")
         float_value_out = None  
     finetuned_prediction = float_value_out
 
     # preview results
-    if debug_mode:
-        print(input_message)
-        print(task_message)
     print(country_name, finetuned_prediction, true_value)
 
-    return country_name, finetuned_prediction, true_value
+    return country_name, finetuned_prediction, true_value, input_message, task_message
 
 '''
 For predicting GDP growth rates
@@ -144,10 +152,16 @@ def query_model_gdp_growth(row, window_start, prep_msg=growth_prep_msg, zero_sho
             model="ft:gpt-3.5-turbo-0613:personal::8KcBXc2t"
         else:
             print("Check parameters") 
-    
+
     # Create sliding window prompts
     i = window_start
     prediction_year = i + predict_after_years
+
+    # messages to user about what exactly is happening
+    if debug_mode:
+        print("Model being used for predictions is ", model)
+        print("Predictions are being made for year ", prediction_year)
+        print("First year for which data is being prompted in is ", window_start)
 
     # GDP growth prompts
     input_message = f"The country of interest is: {country_name} and growth rates in percent of real GDP seen in previous years is {i}: {row[str(i)]}, {i+1}: {row[str(i+1)]}, {i+2}: {row[str(i+2)]}, {i+3}: {row[str(i+3)]}, {i+4}: {row[str(i+4)]}, {i+5}: {row[str(i+5)]}, {i+6}: {row[str(i+6)]}, {i+7}: {row[str(i+7)]}, {i+8}: {row[str(i+8)]}, {i+9}: {row[str(i+9)]}"
@@ -171,24 +185,30 @@ def query_model_gdp_growth(row, window_start, prep_msg=growth_prep_msg, zero_sho
     try:
         float_value_out = float(finetuned_prediction_dict['content'])
     except ValueError:
-        print(f"Error: Invalid value encountered. Could not convert '{input_str}' to a float.")
+        print(f"Error: Invalid value encountered. Could not convert '{finetuned_prediction_dict['content']}' to a float.")
         float_value_out = None  
     finetuned_prediction = float_value_out
 
     # preview results
-    if debug_mode:
-        print(input_message)
-        print(task_message)
     print(country_name, finetuned_prediction, true_value)
 
-    return country_name, finetuned_prediction, true_value
+    return country_name, finetuned_prediction, true_value, input_message, task_message
 
 # loop through all countries and return results
 prediction_idx = 0
+print("Making predictions on dataset ", input_csv_filename)
+print("Writing results to ", output_csv_filename)
+print("Model is being used as agent for: ", prep_msg)
+
 for row in data:
     if prediction_idx < PREDICTION_LIMIT:
-        # country_name, y_hat, y_true = query_model_gdp(row, test_windows_start) # Extract relevant data for that country and start year combination - GDP 
-        country_name, y_hat, y_true = query_model_gdp_growth(row, test_windows_start) # Extract relevant data for that country and start year combination - GDP Growth
+        # country_name, y_hat, y_true, input_message, task_message = query_model_gdp(row, test_windows_start, prep_msg=prep_msg, zero_shot=ZERO_SHOT) # Extract relevant data for that country and start year combination - GDP 
+        country_name, y_hat, y_true, input_message, task_message = query_model_gdp_growth(row, test_windows_start, prep_msg=prep_msg, zero_shot=ZERO_SHOT) # Extract relevant data for that country and start year combination - GDP Growth
+        if prediction_idx == 0:
+           print(input_message)
+           print(task_message)
+           print("First year for which data is being prompted in is ", test_windows_start)
+           print("Zero shot", ZERO_SHOT)
         countries.append(country_name)
         true_values.append(y_true)
         finetuned_predictions.append(y_hat)
